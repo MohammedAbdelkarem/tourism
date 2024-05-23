e81fd65df739431fa46fa7e1513a1d76591ff5bb
MohammedAbdelkarem <mohammedak2048@gmail.com>
2024-05-23 10:03:35 +0300

    clean up the adminauthcontroller , test the logout

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,101 +1,31 @@
 <?php
 namespace App\Http\Controllers;
-use App\Models\User;
-use App\Models\Admin;
+
+use App\Http\Requests\AdminLoginRequest;
 use App\Traits\ResponseTrait;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminController extends Controller
 {
 
     use ResponseTrait;
-    /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
-    public function __construct() {
+    public function __construct()
+    {
         $this->middleware('auth:admin', ['except' => ['login', 'register']]);
     }
-    public function login(Request $request){
-    	$validator = Validator::make($request->all(), [
-            'email' => 'required|email',
-            'password' => 'required|string|min:6',
-        ]);
-        if ($validator->fails()) {
-            // return response()->json($validator->errors(), 422);
-            return $this->SendResponse(response::HTTP_UNPROCESSABLE_ENTITY , $validator->errors());
-        }
-        if (! $token = auth()->attempt($validator->validated())) {
-            // return response()->json(['error' => 'Unauthorized'], 401);
+    public function login(AdminLoginRequest $request)
+    {
+        if (! $token = auth()->attempt($request->only('email' , 'password')))
+        {
             return $this->SendResponse(response::HTTP_UNAUTHORIZED , 'wrong email or password');
         }
-        return $this->createNewToken($token);
-    }
-    /**
-     * Register a User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    // public function register(Request $request) {
-    //     $validator = Validator::make($request->all(), [
-    //         'name' => 'required|string|between:2,100',
-    //         'email' => 'required|string|email|max:100|unique:admins',
-    //         'password' => 'required|string|min:6',
-    //     ]);
-    //     if($validator->fails()){
-    //         return response()->json($validator->errors()->toJson(), 400);
-    //     }
-    //     $user = Admin::create(array_merge(
-    //                 $validator->validated(),
-    //                 ['password' => bcrypt($request->password)]
-    //             ));
-    //     return response()->json([
-    //         'message' => 'User successfully registered',
-    //         'user' => $user
-    //     ], 201);
-    // }
-
-    /**
-     * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function logout() {
-        auth()->logout();
-        return response()->json(['message' => 'User successfully signed out']);
-    }
-    /**
-     * Refresh a token.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function refresh() {
-        return $this->createNewToken(auth()->refresh());
-    }
-    /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function userProfile() {
-        return response()->json(auth()->user());
-    }
-    /**
-     * Get the token array structure.
-     *
-     * @param  string $token
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function createNewToken($token){
-        // return response()->json([
-        //     'token' => $token,
-        //     'user' => auth()->user()
-        // ]);
         return $this->SendResponse(response::HTTP_OK , 'logged in successfully' ,['token' => $token]);
     }
+    public function logout()
+    {
+        auth()->logout();
+        
+        return $this->SendResponse(response::HTTP_OK , 'logged out successfully');
+    }
+    
 }
